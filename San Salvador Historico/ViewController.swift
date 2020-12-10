3742874ac71d7f7c32bf17c43336037099d943bc
Rafael Castro <rcastroluna.sv@gmail.com>
2020-12-09 20:11:03 -0600

    Initial Commit

---
+++ San Salvador Historico/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  San Salvador Historico
+//
+//  Created by Rafael David Castro Luna on 12/9/20.
+//  Copyright © 2020 The Kitchen DS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ San Salvador HistoricoTests/San_Salvador_HistoricoTests.swift
@@ -0,0 +1,34 @@
+//
+//  San_Salvador_HistoricoTests.swift
+//  San Salvador HistoricoTests
+//
+//  Created by Rafael David Castro Luna on 12/9/20.
+//  Copyright © 2020 The Kitchen DS. All rights reserved.
+//
+
+import XCTest
+@testable import San_Salvador_Historico
+
+class San_Salvador_HistoricoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
